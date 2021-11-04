ea2366951a4704e671dce7042f48137dcda667fb
Talipov S.N <proffix4@gmail.com>
2021-11-05 00:08:47 +0600

    Update python_fedora.sh

--- python_fedora.sh
+++ python_fedora.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo dnf install qt5-designer python3-tkinter
+sudo dnf install python3-pytz
 pip install pyqt5 pyqt5-tools
 pip install setuptools numpy pandas
 pip install openpyxl xlrd
