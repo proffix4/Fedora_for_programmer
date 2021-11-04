16b776f0377ab55ee0a0758aac18e893ff6e8ca8
Talipov S.N <proffix4@gmail.com>
2021-11-04 23:30:48 +0600

    Update python_fedora.sh

--- python_fedora.sh
+++ python_fedora.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo dnf install qt5-designer python3-tkinter
-sudo pip3 install pyqt5 pyqt5-tools
-sudo pip3 install setuptools numpy pandas
-sudo pip3 install openpyxl xlrd
-sudo pip3 install python-docx XlsxWriter openpyxl Pillow
-sudo pip3 install ebooklib pykson
+pip install pyqt5 pyqt5-tools
+pip install setuptools numpy pandas
+pip install openpyxl xlrd
+pip install python-docx XlsxWriter openpyxl Pillow
+pip install ebooklib pykson
