5cc39946a5fda458e6087836bbff734e93a69fb0
Talipov S.N <proffix4@gmail.com>
2021-11-04 17:17:50 +0600

    Add files via upload

---
+++ git_fedora.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo dnf install git
---
+++ python_fedora.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo dnf install qt5-designer python3-tkinter
+sudo pip3 install pyqt5 pyqt5-tools
+sudo pip3 install setuptools numpy pandas
+sudo pip3 install openpyxl xlrd
+sudo pip3 install python-docx XlsxWriter openpyxl Pillow
+sudo pip3 install ebooklib pykson
